3161bfe7c578e7ae990c7cdfc40f0f69f4a3e570
Ismailov Bunyod <68915478+BunyodIsmailov@users.noreply.github.com>
2022-05-16 13:18:42 +0500

    Add files via upload

--- tests.py
+++ tests.py
@@ -7,13 +7,17 @@
 
 # pip install requests
 # pip install requests-toolbelt
-
+import time
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
+
+time.sleep(5)
 try:
 	multipart_data = MultipartEncoder(fields={"scanerStatus": "true","file": ("filename.jpg",open("D:/printchi/printchiapp/kalit_suzlar.jpg", 'rb'),"image/jpeg")})
-	requests.put(url="https://api.printchi.uz/scaner/set/3/",headers={"Content-Type": multipart_data.content_type,},data=multipart_data,)
+	r = requests.put(url="https://api.printchi.uz/scaner/set/3/",headers={"Content-Type": multipart_data.content_type,},data=multipart_data,)
+	print(r.status_code)
+	time.sleep(5)
 except:
 	print("Error on PUT Scanner data!")
 
